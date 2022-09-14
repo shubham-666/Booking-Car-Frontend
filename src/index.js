e9e90da680aacc7c802b0041ff216536cc3540c0
SHUBHAM TIWARI <98177201+rocksLionstar@users.noreply.github.com>
2022-09-14 14:29:38 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { Provider } from 'react-redux'
+import Favicon from 'react-favicon'
+ReactDOM.render(
+  <Provider store={store}>
+    <Favicon url='./favicon.ico' />
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
