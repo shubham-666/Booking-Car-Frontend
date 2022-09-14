a36cff6e9698cdb111a69a55819e06a4168b5a6e
SHUBHAM TIWARI <98177201+rocksLionstar@users.noreply.github.com>
2022-09-14 21:45:44 +0530

    added

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -7,7 +7,7 @@ export const userLogin = (reqObj) => async dispatch => {
     dispatch({ type: 'LOADING', payload: true })
 
     try {
-        const response = await axios.post(`/api/users/login`, reqObj)
+        const response = await axios.post(`${url}/users/login`, reqObj)
         localStorage.setItem('user', JSON.stringify(response.data))
         message.success('Login success')
         dispatch({ type: 'LOADING', payload: false })
